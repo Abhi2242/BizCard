41dad7948ec7d0edf26353a4d8c0fbfae93b5c01
Abhishek_GKB <abhishek.vengurlekar@gkbhitech.com>
2026-01-16 10:10:06 +0530

    My Test class added

--- app/src/main/java/com/smartgeek/bizcard/ui/view/MainActivity.kt
+++ app/src/main/java/com/smartgeek/bizcard/ui/view/MainActivity.kt
@@ -3,9 +3,11 @@ package com.smartgeek.bizcard.ui.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -29,8 +31,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -124,20 +128,28 @@ fun Portfolio(data: List<String>) {
                 colors = CardDefaults.cardColors(Color.White),
                 border = BorderStroke(width = 2.dp, color = Color.LightGray)
             ) {
+                var expanded by remember { mutableStateOf(false) }
                 Row(modifier = Modifier
                     .padding(8.dp)
                     .fillMaxWidth()
-                    .background(Color.White)){
-                    CreateProfileImage(modifier = Modifier.size(100.dp))
-                    Column(modifier = Modifier.fillMaxHeight()
-                        .padding(5.dp)
-                        .align(alignment = Alignment.CenterVertically)) {
-                        Text(text = item,
-                            color = Color.Black,
-                            style = Typography.titleMedium)
-                        Text(text = "A Great Project",
-                            color = Color.Black,
-                            style = Typography.bodySmall)
+                    .background(Color.White))
+                {
+                    CreateProfileImage(modifier = Modifier
+                        .size(100.dp)
+                        .clickable { expanded = !expanded })
+                    Column(modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(5.dp)) {
+                        AnimatedVisibility(visible = expanded) {
+                            Column(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
+                                Text(text = item,
+                                    color = Color.Black,
+                                    style = Typography.titleMedium)
+                                Text(text = "A Great Project",
+                                    color = Color.Black,
+                                    style = Typography.bodySmall)
+                            }
+                        }
                     }
                 }
             }
